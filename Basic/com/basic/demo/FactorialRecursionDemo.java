76a6d68366abc55a809299cf30eae3bce495c0fa
Rohit <kumbharrohit13@gmail.com>
2023-10-29 12:34:10 +0530

    Fibonacci series code updated

--- Basic/com/basic/demo/FactorialRecursionDemo.java
+++ Basic/com/basic/demo/FactorialRecursionDemo.java
@@ -1,5 +1,20 @@
 package com.basic.demo;
 
+/**
+ * @author Rohit
+ * 
+ *         Example of fibonacci series in java - 0 1 1 2 3 5 8 13 21 34 55 89.
+ * 
+ *         First number of series is 0 & second number of series is 1. So, logic
+ *         behind the series generation is that the subsequent number generated
+ *         is sum of previous two number of series.
+ * 
+ *         Example of fibonacci series in java - First number of series is 0 &
+ *         second number of series is 1. So, third number will be 0+1=1 So,
+ *         fourth number will be 1+1=2 So, fifth number will be 2+1=3 and so on
+ *
+ */
+
 public class FactorialRecursionDemo {
 	static int factorial;
 
--- Basic/com/basic/demo/FibonacciWithoutRecursion.java
+++ Basic/com/basic/demo/FibonacciWithoutRecursion.java
@@ -1,23 +1,36 @@
-/*Program to implement Fibonnacci Series without using recursion*/
+
 package com.basic.demo;
 
+/**
+ * @author Rohit
+ * 
+ *         Example of fibonacci series in java - 0 1 1 2 3 5 8 13 21 34 55 89.
+ * 
+ *         First number of series is 0 & second number of series is 1. So, logic
+ *         behind the series generation is that the subsequent number generated
+ *         is sum of previous two number of series.
+ * 
+ *         Example of fibonacci series in java - First number of series is 0 &
+ *         second number of series is 1. So, third number will be 0+1=1 So,
+ *         fourth number will be 1+1=2 So, fifth number will be 2+1=3 and so on
+ *
+ */
 public class FibonacciWithoutRecursion {
 
 	public static void main(String[] args) {
-		int n = 12;
+		int n = 10;
 		generateFibonacciSeries(n);
 	}
 
 	private static void generateFibonacciSeries(int n) {
 		int first = 0;
 		int second = 1;
-		System.out.print(first + "\t" + second + "\t");
-		int temp = 0;
-		for (int i = 1; i < n; i++) {
+		int temp;
+		for (int i = 0; i < n; i++) {
 			temp = first + second;
 			first = second;
 			second = temp;
-			System.out.print(temp + "\t");
+			System.out.print(temp + " ");
 		}
 	}
 }
